4589ef4e8503262e4edd3d916f006ec7eebdda2e
Ivan <user-email@go.uk>
2024-05-13 16:36:31 +0300

    add req 4

--- server.py
+++ server.py
@@ -5,7 +5,10 @@ import socket
 HOST = "localhost"  # Standard loopback interface address (localhost)
 PORT = 10000 # Port to listen on (non-privileged ports are > 1023)
 
-print("Start server...")
+
+HOST2 = socket.gethostbyname(socket.gethostname())
+
+print("Start server...", HOST2)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
